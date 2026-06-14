863a5691e3a011049a3c18167addc923ccd5e515
NeerajSangwan <144983325+NeerajSangwan@users.noreply.github.com>
2026-06-14 06:28:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 149.7 MB (50.00%)

---
+++ 4332-check-good-integer/solution.dart
@@ -0,0 +1,15 @@
+class Solution {
+  bool checkGoodInteger(int n) {
+    int digitSum = 0;
+    int squareSum = 0;
+    
+    while (n > 0) {
+      int d = n % 10;
+      digitSum += d;
+      squareSum += d * d;
+      n ~/= 10;
+    }
+    
+    return squareSum - digitSum >= 50;
+  }
+}
