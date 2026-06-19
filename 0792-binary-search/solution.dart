f4b07dfdf239ce159e104e225e6d77e3b1409541
NeerajSangwan <144983325+NeerajSangwan@users.noreply.github.com>
2026-06-19 06:19:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 153.3 MB (98.08%)

---
+++ 0792-binary-search/solution.dart
@@ -0,0 +1,21 @@
+class Solution {
+  int search(List<int> nums, int target) {
+    int left = 0;
+    int right = nums.length-1;
+    int mid = right ~/ 2;
+   while (left <= right){
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+        mid = (left + right) ~/ 2;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+        mid = (left + right) ~/ 2;
+      }
+    }
+    return -1;
+  }
+}
+
+
