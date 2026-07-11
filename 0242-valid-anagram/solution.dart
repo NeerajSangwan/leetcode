414bf7c467bf5342fd0ad4e2328f8e02b9859f68
NeerajSangwan <144983325+NeerajSangwan@users.noreply.github.com>
2026-07-11 12:08:31 +0000

    Sync LeetCode submission Runtime - 46 ms (10.15%), Memory - 152.2 MB (19.80%)

--- 0242-valid-anagram/solution.dart
+++ 0242-valid-anagram/solution.dart
@@ -1,27 +1,26 @@
 class Solution {
   bool isAnagram(String s, String t) {
+    Map<String,int> m = {};
     if(s.length!=t.length){
         return false;
     }
-    Map<String,int> m = {};
     for(int i=0;i<s.length;i++){
         if(m.containsKey(s[i])){
-            m[s[i]] = m[s[i]]! +1;
+            m[s[i]]  = m[s[i]]! +1;
         }else{
             m[s[i]] = 1;
         }
     }
-    for(int i =0;i<t.length;i++){
+    for(int i=0;i<t.length;i++){
         if(m.containsKey(t[i])){
-            m[t[i]] = m[t[i]]!-1;
+            m[t[i]] = m[t[i]]! -1;
         }else{
             return false;
         }
     }
-    for(var num in m.entries){
-        if(num.value != 0){
-        
-            return false;
+    for(var entry in m.entries){
+        if(entry.value!=0){
+           return false;
         }
     }
     return true;
